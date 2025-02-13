3951adf736542e55d39911fdbc018ac0f139f48d
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-02-13 14:38:07 -0300

    separador e seção sobre

--- src/pages/biblioteca/IndexBiblioteca.jsx
+++ src/pages/biblioteca/IndexBiblioteca.jsx
@@ -2,13 +2,12 @@ import Header from "../../components/header/Header";
 import Section from "../../components/section/Section";
 import LinkButton from "../../components/link-button/LinkButton";
 import Slider from "../../components/slider/Slider";
-
+import Separator from "../../components/separator/Separator";
 
 import img1 from "../../assets/mej-1.jpg";
 import img2 from "../../assets/mej-2.jpg";
 import img3 from "../../assets/mej-3.jpg";
 import img4 from "../../assets/mej-4.jpg";
-
 
 import css from "../../components/section/Section.module.css";
 
@@ -50,9 +49,36 @@ const IndexBiblioteca = () => {
             <LinkButton text="Alugar livro" link="/biblioteca#alugar" />
           </article>
           <div className={css.area}>
-            <Slider imgs={imgs}/>
+            <Slider imgs={imgs} />
           </div>
         </Section>
+        <Separator>
+          <q>
+            Só se <strong>ama</strong> aquilo que se <strong>conhece</strong>
+          </q>
+        </Separator>
+        <Section>
+          <article className={css.area}>
+            <h2>Sobre o projeto</h2>
+            <p>
+              A biblioteca do MEJ busca tornar presente no dia-a-dia dos jovens
+              o hábito da leitura, e também conhecimento acerca da beleza dos
+              elementos importantíssimos para a fé católica, como a Sagrada
+              Tradição, Sagrada Escritura e o Sagrado Magistério, oferecendo
+              livros que os ajudarão em seu crescimento espiritual.
+            </p>
+          </article>
+          <div className={css.area}>
+            <img
+              src={img2}
+              width="350px"
+              alt="Foto tirada em encontro do mej"
+            />
+          </div>
+        </Section>
+        <Separator>
+          <q><strong>Creio</strong> para entender e <strong>entendo</strong> para crer</q>
+        </Separator>
       </main>
     </>
   );

63c781c0b63abc191658acc62eea6d41353ee0d3
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-02-14 08:57:27 -0300

    remove process

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,3 @@
-const {
-  REACT_APP_apiKey,
-  REACT_APP_authDomain,
-  REACT_APP_projectId,
-  REACT_APP_storageBucket,
-  REACT_APP_messagingSenderId,
-  REACT_APP_appId,
-} = process.env;
-
 import { initializeApp } from "firebase/app";
 import {
   getAuth,

f8a31109a542ac995e58c42cbb65f6868d7e78ea
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-02-18 20:15:14 -0300

    Atualizações na página de administração de livros

--- src/pages/biblioteca/LivrosAdm.jsx
+++ src/pages/biblioteca/LivrosAdm.jsx
@@ -6,6 +6,11 @@ import Header from "../../components/header/Header";
 import Section from "../../components/section/Section";
 import CatalogControls from "../../components/CatalogControls/CatalogControls";
 
+import addIcon from "../../assets/add.svg";
+import editIcon from "../../assets/edit.svg";
+import borrowIcon from "../../assets/borrow.svg";
+import deleteIcon from "../../assets/delete.svg";
+
 const LivrosAdm = () => {
   const { user } = useAuth();
   const [AllBooks, setAllBooks] = useState([]);
@@ -43,6 +48,8 @@ const LivrosAdm = () => {
       <main>
         <Section>
           <h1>Oi {user.displayName || "aa"}!</h1>
+        </Section>
+        <Section>
           <p>O que deseja fazer...?</p>
         </Section>
         <Section>
@@ -52,15 +59,57 @@ const LivrosAdm = () => {
           />
         </Section>
         <Section>
-          <ul className="catalogo">
-            {loading ? (
-              <p>Carregando...</p>
-            ) : AllBooks.length > 0 ? (
-              filteredBooks.map((b, i) => <li key={i}>{b.name}</li>)
-            ) : (
-              <li>Nenhum livro encontrado</li>
-            )}
-          </ul>
+          <div className="adm-buttons add-book">
+            <button id="create">
+              <img src={addIcon} alt="Ícone de mais" />
+            </button>
+            <span>Cadastrar novo livro</span>
+          </div>
+        </Section>
+        <Section>
+          {loading && <p style={{width: "100%", textAlign: "left"}}>Carregando...</p>}
+
+          {!loading && AllBooks.length === 0 && (
+            <p style={{width: "100%", textAlign: "left"}}>Parece que a biblioteca está sem livros...</p>
+          )}
+
+          {!loading && AllBooks.length > 0 && results === 0 && (
+            <p style={{width: "100%", textAlign: "left"}}>Não tem livros para esse filtro...</p>
+          )}
+
+          {!loading && AllBooks.length > 0 && results > 0 && (
+            <ul className="catalog">
+              {filteredBooks.map((b, i) => (
+                <li key={i}>
+                  <div className="book-header">
+                    <div className="book-cover"></div>
+                    <div>
+                      <div className="name">
+                        {b.name}, por {b.author}
+                      </div>
+                      {b.collection && (
+                        <div className="collection">
+                          {b.collection.title} - Vol. {b.collection.volume}
+                        </div>
+                      )}
+                      <div className="adm-buttons">
+                        <button id="edit">
+                          <img src={editIcon} alt="ìcone de lápis" />
+                        </button>
+                        <button id="borrow">
+                          <img src={borrowIcon} alt="ìcone de enviar" />
+                        </button>
+                        <button id="delete">
+                          <img src={deleteIcon} alt="ìcone de lixeira" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="description">{b.description}</div>
+                </li>
+              ))}
+            </ul>
+          )}
         </Section>
         <button onClick={logOut}>Sair</button>
       </main>

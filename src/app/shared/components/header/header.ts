7a60bed527a7922b8afbe7d3617395c86e1673b9
Yago Pontes <yagopontesf14@gmail.com>
2026-05-20 12:57:09 -0300

    feat: aparecer e desaparecer menu

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { Menu } from './components/menu/menu'; 
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Menu],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   logoPath = 'logo/logo-mej.png';
+  showMenu: boolean = false;
+
+  toogleMenuButton(){
+    this.showMenu = !this.showMenu;
+  }
 }

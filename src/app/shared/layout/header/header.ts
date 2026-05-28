44d384da36aa668b60384fa57b712eb9adb22b30
Ramon Silva <ramonerik29a@gmail,com>
2026-05-28 12:50:45 -0300

    feat: moving img path from variable to html

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-  logoPath = 'logo/logo-mej.png';
-}
+export class Header {}

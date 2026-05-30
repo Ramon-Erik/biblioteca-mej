87d31f9360f75707954197b01454e3a372fb7047
Ramon Silva <ramonerik29a@gmail,com>
2026-05-30 12:35:48 -0300

    feat: links for each menu item

--- src/app/shared/layout/header/components/menu/menu.ts
+++ src/app/shared/layout/header/components/menu/menu.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
 export class Menu {
-  public isOpen = false;
-
-  toggleMenu() {
-    this.isOpen = !this.isOpen;
-  }
+  public navLinks = [
+    {
+      icon: 'bi-person-fill',
+      path: '/perfil',
+    },
+    {
+      icon: 'bi-house-door-fill',
+      path: '/home',
+    },
+    {
+      icon: 'bi-book-fill',
+      path: '/catalogo-de-livros',
+    },
+  ];
 }

36b49e0206469a79e28a1e56d1bc2b0009136734
Ramon Silva <ramonerik29a@gmail,com>
2026-05-30 15:26:56 -0300

    feat: main layout display and catalog route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/leitor/leitor.routes').then((m) => m.routes),
+    pathMatch: 'full',
+    loadComponent: () => import('./modules/leitor/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'catalogo-de-livros',
+    pathMatch: 'full',
+    loadComponent: () => import('./modules/leitor/catalog/catalog').then((m) => m.Catalog),
   },
 ];

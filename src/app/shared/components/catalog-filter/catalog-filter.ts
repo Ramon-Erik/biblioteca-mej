1c717cca6b0f0fa7e01b25dc6c903d4f23c8b2a4
Ramon Silva <ramonerik29a@gmail,com>
2026-06-02 09:56:29 -0300

    feat: implement dynamic result text in catalog filter component

--- src/app/shared/components/catalog-filter/catalog-filter.ts
+++ src/app/shared/components/catalog-filter/catalog-filter.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit, DestroyRef } from '@angular/core';
+import { ReaderCatalog } from '../../../modules/leitor/catalog/service/reader-catalog';
+import { map } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-catalog-filter',
@@ -6,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './catalog-filter.html',
   styleUrl: './catalog-filter.scss',
 })
-export class CatalogFilter {
+export class CatalogFilter implements OnInit {
+  private catalogService = inject(ReaderCatalog);
+  private destroyRef = inject(DestroyRef);
+  public responsesText = signal('');
 
+  private formatResultsText(l: number) {
+    console.log(l, 'aa');
+
+    if (l == 0) return 'Nenhum resultado.';
+    if (l == 1) return 'Um resultado.';
+    return l + ' resultados.';
+  }
+
+  ngOnInit() {
+    this.catalogService.catalogLength
+      .pipe(takeUntilDestroyed(this.destroyRef), map(this.formatResultsText))
+      .subscribe((res) => this.responsesText.set(res));
+  }
 }

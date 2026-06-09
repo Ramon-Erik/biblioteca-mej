7e2082d5aed8dad9709ae23d03056ff9097c496a
Ramon Silva <ramonerik29a@gmail,com>
2026-06-09 13:03:24 -0300

    feat: new book form

--- src/app/modules/admin/catalog/components/modal/register-book/register-book.ts
+++ src/app/modules/admin/catalog/components/modal/register-book/register-book.ts
@@ -1,12 +1,32 @@
 import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { InputDefault } from '@shared/components/input/input';
 
 @Component({
   selector: 'app-register-book',
-  imports: [],
+  imports: [ReactiveFormsModule, InputDefault],
   templateUrl: './register-book.html',
   styleUrl: './register-book.scss',
 })
 export class RegisterBook {
   public activeModal = inject(NgbActiveModal);
+  private fb = inject(FormBuilder);
+
+  public bookForm = new FormGroup({
+    nomeObra: new FormControl('', [Validators.required]),
+    autor: new FormControl('', [Validators.required]),
+    editora: new FormControl(''),
+    volume: new FormControl(''),
+    descricao: new FormControl(''),
+    categoriasIds: new FormControl([]),
+    quantidade: new FormControl(''),
+    fotoCapaUrl: new FormControl(''),
+  });
 }

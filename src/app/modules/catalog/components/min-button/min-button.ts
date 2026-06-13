8f8b1c5e98fe53771f88fa46371e874ccfc7c900
Ramon Silva <ramonerik29a@gmail,com>
2026-06-13 11:55:18 -0300

    feat: changing icon on min-button

--- src/app/modules/catalog/components/min-button/min-button.ts
+++ src/app/modules/catalog/components/min-button/min-button.ts
@@ -6,9 +6,11 @@ import { BlurOnClick } from '@shared/directives/blur-on-click';
   imports: [BlurOnClick],
   templateUrl: './min-button.html',
   styleUrl: './min-button.scss',
+  standalone: true,
 })
 export class MinButton {
-  color = input<string>('var(--bg-btn-add)');
+  public color = input<string>('var(--bg-btn-add)');
+  public icon = input.required<string>();
 
   // (ex: '2.5rem', '40px')
   size = input<string>('1.9rem');
